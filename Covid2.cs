9b451a561d965c7a100b2b8bca5eda7be38fe711
Amre Amer <me@amre-amer.com>
2020-04-23 14:06:26 -0700

    Code for flattening the curve
    https://sites.google.com/amre-amer.com/resume/home/covid?authuser=0

---
+++ Covid2.cs
@@ -0,0 +1,470 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Scripting.APIUpdating;
+
+public class Person
+{
+    public GameObject go;
+    public int dayInfected;
+    public bool ynInfected;
+    public bool ynImmune;
+    public int othersInfected;
+    public Person(GameObject go0)
+    {
+        go = go0;
+    }
+}
+
+public class Covid2 : MonoBehaviour
+{
+    int numPersons;
+    float side;
+    float scalePerson;
+    float speed;
+    float nearDist;
+    int daysInfected;
+    Color colorInfected;
+    Color colorImmune;
+    Color colorNotInfected;
+    List<Person> persons;
+    GameObject goPersons;
+    GameObject goPersonPrefab;
+    GameObject goBorders;
+    GameObject goBorderPrefab;
+    GameObject goGraphs;
+    GameObject goGraphPrefab;
+    GameObject goInfo;
+    GameObject goGraphNotInfectedLast;
+    GameObject goGraphInfectedLast;
+    GameObject goGraphImmuneLast;
+    int day;
+    int cntInfected;
+    int cntImmune;
+    int cntNotInfected;
+    int cntMaxInfected;
+    bool ynDone;
+    int numOthersInfected;
+    int numOthersInfectedDenominator;
+    int dayLockDownEnds;
+    float lockDownIncrease;
+    int cycle;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        numPersons = 500;
+        side = 200;
+        scalePerson = 3;
+        speed = 2.25f;
+        nearDist = scalePerson;
+        daysInfected = 21;
+        dayLockDownEnds = 90;
+        lockDownIncrease = 1.5f;
+        //
+        persons = new List<Person>();
+        colorNotInfected = Color.white;
+        colorInfected = Color.red;
+        colorImmune = Color.green;
+        goPersons = GameObject.Find("Persons");
+        goPersonPrefab = GameObject.Find("Person");
+        goBorders = GameObject.Find("Borders");
+        goBorderPrefab = GameObject.Find("Border");
+        goGraphs = GameObject.Find("Graphs");
+        goGraphPrefab = GameObject.Find("Graph");
+        goInfo = GameObject.Find("Info");
+        LoadBorders();
+        LoadPersons();
+        Infect();
+    }
+
+    void Infect()
+    {
+        int n = UnityEngine.Random.Range(0, numPersons);
+        n = persons.Count - 1;
+        persons[n].go.transform.position = new Vector3(side * .75f, 0, side/2);
+        SetColor(persons[n].go, colorInfected);
+        persons[n].dayInfected = day;
+        persons[n].ynInfected = true;
+    }
+
+    void LoadPersons() {
+        for(int n = 0; n < numPersons; n++)
+        {
+            float x = UnityEngine.Random.Range(0, side - scalePerson);
+            float z = UnityEngine.Random.Range(0, side - scalePerson);
+            Vector3 pos = new Vector3(x, 0, z);
+            LoadPerson(pos);
+        }
+        //Destroy(goPersonPrefab);
+        Debug.Log("LoadPersons()\n");
+    }
+
+    Person LoadPerson(Vector3 pos)
+    {
+        GameObject go = Instantiate(goPersonPrefab, goPersons.transform);
+        go.transform.position = pos;
+        go.transform.localScale = Vector3.one * scalePerson;
+        go.transform.Rotate(0, UnityEngine.Random.Range(0, 360), 0);
+        Person person = new Person(go);
+        persons.Add(person);
+        SetColor(go, colorNotInfected);
+        return person;
+    }
+
+    void LoadBorders()
+    {
+        for (int n = 0; n < 5; n++)
+        {
+            GameObject go;
+            go = Instantiate(goBorderPrefab, goBorders.transform);
+            Vector3 pos = Vector3.zero;
+            Vector3 sca = Vector3.zero;
+            float thick = 1f;
+            switch (n)
+            {
+                case 0:
+                    pos = new Vector3(side/2, 0, 0);
+                    sca = new Vector3(side, thick, thick);
+                    break;    
+                case 1:
+                    pos = new Vector3(side, 0, side/2);
+                    sca = new Vector3(thick, thick, side);
+                    break;
+                case 2:
+                    pos = new Vector3(side/2, 0, side);
+                    sca = new Vector3(side, thick, thick);
+                    break;
+                case 3:
+                    pos = new Vector3(0, 0, side/2);
+                    sca = new Vector3(thick, thick, side);
+                    break;
+                case 4:
+                    pos = new Vector3(side / 2, 0, side / 2);
+                    sca = new Vector3(thick, thick, side);
+                    break;
+                default:
+                    break;
+            }
+            go.transform.position = pos;
+            go.transform.localScale = sca;
+        }
+        //Destroy(goBorderPrefab);
+    }
+
+    void Move()
+    {
+        foreach(Person person in persons)
+        {
+            GameObject go = person.go;
+            go.transform.position += go.transform.forward * speed;
+            Bounce(person);
+        }
+    }
+
+    void Bounce(Person personCheck)
+    {
+        GameObject goCheck = personCheck.go;
+        if (personCheck.ynInfected)
+        {
+            if (day - personCheck.dayInfected > daysInfected)
+            {
+                personCheck.ynInfected = false;
+                personCheck.ynImmune = true;
+                SetColor(goCheck, colorImmune);
+            }
+        }
+        foreach (Person person in persons)
+        {
+            GameObject go = person.go;
+            if (go != goCheck)
+            {
+                if (IsNearPerson(goCheck, go))
+                {
+                    if (personCheck.ynInfected && !person.ynInfected && !person.ynImmune)
+                    {
+                        person.dayInfected = day;
+                        person.ynInfected = true;
+                        SetColor(person.go, colorInfected);
+                        personCheck.othersInfected++;
+                        personCheck.go.name = personCheck.othersInfected.ToString();
+                    }
+                    ReflectPerson(personCheck, person);
+                }
+            }
+        }
+        IsNearBorderReflect(goCheck);
+    }
+
+    void SetColor(GameObject go, Color color)
+    {
+        go.GetComponent<Renderer>().material.color = color;
+    }
+
+    void IsNearBorderReflect(GameObject goCheck)
+    {
+        Vector3 posCheck = goCheck.transform.position;
+        Vector3 eulCheck = goCheck.transform.eulerAngles;
+        for (int n = 0; n < goBorders.transform.childCount; n++)
+        {
+            GameObject go = goBorders.transform.GetChild(n).gameObject;
+            Vector3 pos = go.transform.position;
+            switch (n)
+            {
+                case 0: // bottom
+                    if (posCheck.z <= pos.z + scalePerson)
+                    {
+                        eulCheck.y += 180;
+                        goCheck.transform.eulerAngles = eulCheck;
+                    }
+                    break;
+                case 1: // right
+                    if (posCheck.x >= pos.x - scalePerson)
+                    {
+                        eulCheck.y += 180;
+                        goCheck.transform.eulerAngles = eulCheck;
+                    }
+                    break;
+                case 2: // up
+                    if (posCheck.z >= pos.z - scalePerson)
+                    {
+                        eulCheck.y += 180;
+                        goCheck.transform.eulerAngles = eulCheck;
+                    }
+                    break;
+                case 3: // left
+                    if (posCheck.x <= pos.x + scalePerson)
+                    {
+                        eulCheck.y += 180;
+                        goCheck.transform.eulerAngles = eulCheck;
+                    }
+                    break;
+                case 4: // middle
+                    if (day < dayLockDownEnds)
+                    {
+                        if (posCheck.x >= pos.x - scalePerson && posCheck.x <= pos.x + scalePerson)
+                        {
+                            if (posCheck.z >= pos.z - side / 2 && posCheck.z <= pos.z + side / 2)
+                            {
+                                eulCheck.y += 180;
+                                goCheck.transform.eulerAngles = eulCheck;
+                            }
+                        }
+                    }
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+
+    bool IsNearPerson(GameObject goCheck, GameObject go)
+    {
+        float dist = Vector3.Distance(goCheck.transform.position, go.transform.position);
+        if (dist <= nearDist)
+        {
+            return true;
+        }
+        return false;
+    }
+
+    void ReflectPerson(Person personCheck, Person person)
+    {
+        GameObject goCheck = personCheck.go;
+        GameObject go = person.go;
+        Vector3 pos = goCheck.transform.position;
+        goCheck.transform.position = go.transform.position;
+        goCheck.transform.LookAt(pos);
+        goCheck.transform.position = pos;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (ynDone) return;
+        Move();
+        Graph();
+        if (day == dayLockDownEnds)
+        {
+            GameObject go = goBorders.transform.GetChild(4).gameObject;
+            go.SetActive(false);
+//            speed *= 1.25f;
+        }
+        day++;
+    }
+
+    void Graph()
+    {
+        GetCounts();
+        //
+        GameObject go;
+        Vector3 sca;
+        Vector3 pos;
+        //
+        go = Instantiate(goGraphPrefab, goGraphs.transform);
+        go.name = "not infected";
+        pos = new Vector3(side + day, 0, 0 + cntNotInfected/2f);
+        sca = new Vector3(1, 1, 2);
+        go.transform.position = pos;
+        if (goGraphNotInfectedLast != null)
+        {
+            go.transform.position = (goGraphNotInfectedLast.transform.position + pos) / 2;
+            go.transform.LookAt(pos);
+            sca.z = Vector3.Distance(pos, goGraphNotInfectedLast.transform.position);
+        }
+        go.transform.localScale = sca;
+        SetColor(go, colorNotInfected);
+        goGraphNotInfectedLast = go;
+        //
+        go = Instantiate(goGraphPrefab, goGraphs.transform);
+        go.name = "infected";
+        pos = new Vector3(side + day, 0, 0 + cntInfected / 2f);
+        sca = new Vector3(1, 1, 2);
+        go.transform.position = pos;
+        if (goGraphInfectedLast != null)
+        {
+            go.transform.position = (goGraphInfectedLast.transform.position + pos) / 2;
+            go.transform.LookAt(pos);
+            sca.z = Vector3.Distance(pos, goGraphInfectedLast.transform.position);
+        }
+        go.transform.localScale = sca;
+        SetColor(go, colorInfected);
+        goGraphInfectedLast = go;
+        //
+        go = Instantiate(goGraphPrefab, goGraphs.transform);
+        go.name = "immune";
+        pos = new Vector3(side + day, 0, 0 + cntImmune / 2f);
+        sca = new Vector3(1, 1, 2);
+        go.transform.position = pos;
+        if (goGraphImmuneLast != null)
+        {
+            go.transform.position = (goGraphImmuneLast.transform.position + pos) / 2;
+            go.transform.LookAt(pos);
+            sca.z = Vector3.Distance(pos, goGraphImmuneLast.transform.position);
+        }
+        go.transform.localScale = sca;
+        SetColor(go, colorImmune);
+        goGraphImmuneLast = go;
+        //
+        float percentNotInfected = cntNotInfected / (float)numPersons * 100;
+        float percentInfected = cntInfected / (float)numPersons * 100;
+        float percentImmune = cntImmune / (float)numPersons * 100;
+        float percentMaxInfected = cntMaxInfected / (float)numPersons * 100;
+        float month = day / 30f;
+        float aveOthersInfected = numOthersInfected / (float)numOthersInfectedDenominator;
+        string txt = "";
+        txt += "not infected " + percentNotInfected.ToString("F2") + " %\n";
+        txt += "infected " + percentInfected.ToString("F2") + " % + (" + percentMaxInfected.ToString("F2") + " %)\n";
+        txt += "immune " + percentImmune.ToString("F2") + " %\n";
+        txt += "month " + month.ToString("F1") + "\n";
+        txt += "days sick " + daysInfected + "\n";
+        txt += "R factor " + aveOthersInfected.ToString("F2") + "\n";
+        txt += "dayLockDownEnds " + dayLockDownEnds; // + " (increase " + ((lockDownIncrease - 1) * 100).ToString("F0") + " %)\n";
+//        Debug.Log(txt);
+        goInfo.GetComponent<TextMesh>().text = txt;
+        //
+        if (day % 30 == 0)
+        {
+            go = Instantiate(goGraphPrefab, goGraphs.transform);
+            go.name = "month";
+            pos = new Vector3(side + day, 0, 0);
+            sca = new Vector3(1, 1, 10);
+            go.transform.position = pos;
+            go.transform.localScale = sca;
+            SetColor(go, Color.green);
+        }
+        if (day == dayLockDownEnds)
+        {
+            go = Instantiate(goGraphPrefab, goGraphs.transform);
+            go.name = "dayLockDownEnd";
+            pos = new Vector3(side + day, 0, 0);
+            sca = new Vector3(4, 1, 10);
+            go.transform.position = pos;
+            go.transform.localScale = sca;
+            SetColor(go, Color.red);
+        }
+    }
+
+    void GetCounts()
+    {
+        cntNotInfected = 0;
+        cntInfected = 0;
+        cntImmune = 0;
+        numOthersInfected = 0;
+        numOthersInfectedDenominator = 0;
+        foreach (Person person in persons)
+        {
+            if (!person.ynInfected && !person.ynImmune)
+            {
+                cntNotInfected++;
+            }
+            else
+            {
+                if (person.ynInfected)
+                {
+                    cntInfected++;
+                    if (cntInfected > cntMaxInfected)
+                    {
+                        cntMaxInfected = cntInfected;
+                    }
+                }
+                else
+                {
+                    if (person.ynImmune)
+                    {
+                        cntImmune++;
+                    }
+                }
+            }
+            if (person.othersInfected > 0)
+            {
+                numOthersInfected += person.othersInfected;
+                numOthersInfectedDenominator++;
+            }
+        }
+        if (cntInfected == 0)
+        {
+            ynDone = true;
+            DateTime dt = DateTime.UtcNow;
+            string txt = "Covid " + dt.ToString("yyyy-MM-dd-HH-mm-ss");
+            ScreenCapture.CaptureScreenshot(txt);
+            Debug.Log("saving image " + txt + "\n");
+            Invoke("Restart", 5);
+        }
+    }
+
+    void Restart()
+    {
+        day = 0;
+        cntMaxInfected = 0;
+        for(int n = 0; n < persons.Count; n++)
+        {
+            Person person = persons[n];
+            Destroy(person.go);
+        }
+        persons.Clear();
+        //
+        for(int n = 0; n < goBorders.transform.childCount; n++)
+        {
+            GameObject go = goBorders.transform.GetChild(n).gameObject;
+            Destroy(go);
+        }
+        //
+        if (cycle % 5 == 0)
+        {
+            for (int n = 0; n < goGraphs.transform.childCount; n++)
+            {
+                GameObject go = goGraphs.transform.GetChild(n).gameObject;
+                Destroy(go);
+            }
+        }
+        goGraphImmuneLast = null;
+        goGraphNotInfectedLast = null;
+        goGraphInfectedLast = null;
+        //
+        cycle++;
+        Start();
+        ynDone = false;
+    }
+}
---
+++ Covid3.cs
@@ -0,0 +1,203 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Covid3 : MonoBehaviour
+{
+    float cntInfected;
+    float cntInfectedNew;
+    float cntInfected2;
+    float cntInfectedNew2;
+    float cntNotInfected;
+    float cntImmune;
+    float cntPopulation;
+    float day;
+    float dayDelta;
+    float rFactor;
+    float rFactor2;
+    float interval;
+    float graphWidth;
+    float intervalCycle;
+    float graphHeight;
+    float cntInfectedNewMax;
+    float cntInfectedNewMax2;
+    GameObject goNotInfected;
+    GameObject goInfected;
+    GameObject goInfectedNew;
+    GameObject goImmune;
+    GameObject goGraphs;
+    GameObject goGraphBackground;
+    GameObject goGraphInfected;
+    GameObject goGraphInfectedNew;
+    GameObject goGraphNotInfected;
+    GameObject goGraphImmune;
+    TextMesh tmNotInfected;
+    TextMesh tmInfected;
+    TextMesh tmInfectedNew;
+    TextMesh tmImmune;
+    TextMesh tmDay;
+    TextMesh tmRfactor;
+    TextMesh tmRfactor2;
+    TextMesh tmPopulation;
+    Vector3 posInfected;
+    Vector3 posInfectedNew;
+    Vector3 posNotInfected;
+    Vector3 posImmune;
+    Vector3 posInfectedLast;
+    Vector3 posInfectedNewLast;
+    Vector3 posNotInfectedLast;
+    Vector3 posImmuneLast;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        day = 0;
+        dayDelta = 1f;
+        interval = .15f;
+        cntPopulation = 328000000; // 728000; // 328000000;
+        graphWidth = 50;
+        intervalCycle = 3;
+        rFactor = 1.53f; // 2.63f; 
+        rFactor2 = 1.9f; // 2.63f; 
+        cntInfected = 1;
+        cntInfected2 = 1;
+        cntNotInfected = cntPopulation;
+        cntImmune = 0;
+        cntInfectedNewMax = 0;
+        cntInfectedNewMax2 = 0;
+        //
+        goNotInfected = GameObject.Find("NotInfected");
+        goInfected = GameObject.Find("Infected");
+        goInfectedNew = GameObject.Find("InfectedNew");
+        goImmune = GameObject.Find("Immune");
+        //
+        goGraphInfected = GameObject.Find("GraphInfected");
+        goGraphInfectedNew = GameObject.Find("GraphInfectedNew");
+        goGraphNotInfected = GameObject.Find("GraphNotInfected");
+        goGraphImmune = GameObject.Find("GraphImmune");
+        goGraphs = GameObject.Find("Graphs");
+        goGraphBackground = GameObject.Find("GraphBackground");
+        //
+        tmNotInfected = GameObject.Find("LabNotInfected").GetComponent<TextMesh>();
+        tmInfected = GameObject.Find("LabInfected").GetComponent<TextMesh>();
+        tmInfectedNew = GameObject.Find("LabInfectedNew").GetComponent<TextMesh>();
+        tmImmune = GameObject.Find("LabImmune").GetComponent<TextMesh>();
+        tmDay = GameObject.Find("LabDay").GetComponent<TextMesh>();
+        tmRfactor = GameObject.Find("LabRfactor").GetComponent<TextMesh>();
+        tmRfactor2 = GameObject.Find("LabRfactor2").GetComponent<TextMesh>();
+        tmPopulation = GameObject.Find("LabPopulation").GetComponent<TextMesh>();
+        //
+        tmPopulation.text = "population " + cntPopulation.ToString("N0");
+        graphHeight = goGraphBackground.transform.localScale.z;
+        //
+        Regen();
+    }
+
+    void Regen()
+    {
+        tmRfactor.text = "Rfactor " + (rFactor + 1).ToString("F2");
+        tmRfactor2.text = "Rfactor2 " + (rFactor2 + 1).ToString("F2");
+        //
+        float cntInfectedLast = cntInfected;
+        cntInfected += (rFactor - 1) * (1 - cntInfected / cntPopulation) * cntInfected * dayDelta;
+        cntInfectedNew = cntInfected - cntInfectedLast;
+        if(cntInfectedNew > cntInfectedNewMax)
+        {
+            cntInfectedNewMax = cntInfectedNew;
+        }
+        //
+        float cntInfectedLast2 = cntInfected2;
+        cntInfected2 += (rFactor2 - 1) * (1 - cntInfected2 / cntPopulation) * cntInfected2 * dayDelta;
+        cntInfectedNew2 = cntInfected2 - cntInfectedLast2;
+        if (cntInfectedNew2 > cntInfectedNewMax2)
+        {
+            cntInfectedNewMax2 = cntInfectedNew2;
+        }
+        //
+        cntNotInfected = cntPopulation - cntInfected;
+        cntImmune = cntInfected;
+        //
+        AdjustBar(goInfected, cntInfected);
+        AdjustBar(goInfectedNew, cntInfectedNew);
+        AdjustBar(goImmune, cntImmune);
+        AdjustBar(goNotInfected, cntNotInfected);
+        //
+        tmNotInfected.text = cntNotInfected.ToString("N0") + " Not Infected";
+        tmInfected.text = cntInfected.ToString("N0") + " Infected";
+        tmInfectedNew.text = cntInfectedNew.ToString("N0") + " Infected New";
+        tmImmune.text = cntImmune.ToString("N0") + " Immune";
+        tmDay.text = "Day " + day.ToString("F0");
+        GameObject go;
+        //
+        go = Instantiate(goGraphInfected, goGraphs.transform);
+        posInfected = getGraphPos(cntInfectedNew2);
+        AdjustGraphLine(go, posInfectedLast, posInfected);
+        //
+        go = Instantiate(goGraphInfectedNew, goGraphs.transform);
+        posInfectedNew = getGraphPos(cntInfectedNew);
+        AdjustGraphLine(go, posInfectedNewLast, posInfectedNew);
+        //
+        go = Instantiate(goGraphNotInfected, goGraphs.transform);
+        posNotInfected = getGraphPos(cntNotInfected);
+        AdjustGraphLine(go, posNotInfectedLast, posNotInfected);
+        //
+        go = Instantiate(goGraphImmune, goGraphs.transform);
+        posImmune = getGraphPos(cntImmune);
+        AdjustGraphLine(go, posImmuneLast, posImmune);
+        //
+        posNotInfectedLast = posNotInfected;
+        posInfectedLast = posInfected;
+        posInfectedNewLast = posInfectedNew;
+        posImmuneLast = posImmune;
+        //
+        if (Mathf.RoundToInt(cntInfectedNew) == 0)
+        {
+            Debug.Log("cntInfectedNewMax " + cntInfectedNewMax.ToString("N0") + "\n");
+            Debug.Log("percent " + (cntInfectedNewMax / cntPopulation * 100).ToString("F1") + "\n");
+            Debug.Log("cntInfectedNewMax2 " + cntInfectedNewMax2.ToString("N0") + "\n");
+            Debug.Log("percent2 " + (cntInfectedNewMax2 / cntPopulation * 100).ToString("F1") + "\n");
+            Invoke("Restart", intervalCycle);
+            return;
+        }
+        day += dayDelta;
+        Invoke("Regen", interval);
+    }
+
+    Vector3 getGraphPos(float cnt)
+    {
+        Vector3 pos = new Vector3(day, 1, -graphHeight + cnt / cntPopulation * graphHeight);
+        return pos;
+    }
+
+    void AdjustBar(GameObject go, float cnt)
+    {
+        Vector3 pos = go.transform.position;
+        Vector3 sca = go.transform.localScale;
+        sca.x = cnt / cntPopulation * graphWidth;
+        if (sca.x < .1f) sca.x = .1f;
+        pos.x = sca.x / 2;
+        go.transform.position = pos;
+        go.transform.localScale = sca;
+    }
+
+    void AdjustGraphLine(GameObject go, Vector3 posLast, Vector3 pos)
+    {
+        if (day == 0)
+        {
+            go.transform.position = pos;
+            go.transform.localScale = new Vector3(.5f, 1, .5f);
+            return;
+        }
+        go.transform.position = posLast;
+        go.transform.LookAt(pos);
+        go.transform.position = (pos + posLast) / 2;
+        go.transform.localScale = new Vector3(.5f, 1, Vector3.Distance(pos, posLast));
+    }
+
+    private void Restart()
+    {
+        Destroy(goGraphs);
+        goGraphs = new GameObject("Graphs");
+        Invoke("Start", interval);
+    }
+}
